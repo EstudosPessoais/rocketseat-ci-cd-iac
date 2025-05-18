44749eb7af49e272ca4c0b336fac8cb879eef555
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-05-18 00:39:25 -0300

    new iam

--- iam.tf
+++ iam.tf
@@ -58,6 +58,12 @@ resource "aws_iam_role_policy" "tf-policy" {
         Action   = "iam:*"
         Effect   = "Allow"
         Resource = "*"
+      },
+      {
+        Sid      = "Statement3"
+        Action   = "s3:*"
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
   })
--- main.tf
+++ main.tf
@@ -5,8 +5,34 @@ terraform {
       version = "5.97.0"
     }
   }
+  backend "s3" {
+    bucket = "rocketseat-bielb1b2-iac"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "rocketseat-bielb1b2-iac"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "rocketseat-bielb1b2-iac"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
